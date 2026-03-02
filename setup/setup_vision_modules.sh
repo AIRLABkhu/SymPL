b60e0966bea17b8fdecea93ded74efaa2aa7b6bf
katehoya <149954627+katehoya@users.noreply.github.com>
2026-03-02 16:15:26 +0900

    Update setup_vision_modules.sh

--- setup/setup_vision_modules.sh
+++ setup/setup_vision_modules.sh
@@ -15,14 +15,14 @@ cd ..
 echo "* [INFO] Installed Grounding DINO"
 
 # Depth Pro
-#git clone https://github.com/apple/ml-depth-pro.git
+git clone https://github.com/apple/ml-depth-pro.git
 cd ml-depth-pro
 pip install -e .
 cd ..
 echo "* [INFO] Installed Depth Pro"
 
 # Orient-Anything
-#git clone https://github.com/SpatialVision/Orient-Anything.git
+git clone https://github.com/SpatialVision/Orient-Anything.git
 mv Orient-Anything orient_anything
 echo "* [INFO] Cloned Orient-Anything"
 
@@ -34,22 +34,22 @@ cd ../../../
 # ================================
 
 # make sure the checkpoint folder exists
-#mkdir -p $CHECKPOINT_DIR
+mkdir -p $CHECKPOINT_DIR
 
 # SAM (https://github.com/facebookresearch/segment-anything#model-checkpoints)
-#wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -P $CHECKPOINT_DIR
-#echo "* [INFO] Downloaded SAM checkpoint"
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -P $CHECKPOINT_DIR
+echo "* [INFO] Downloaded SAM checkpoint"
 
 # Grounding DINO (https://github.com/IDEA-Research/GroundingDINO)
-#wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth -P $CHECKPOINT_DIR
-#echo "* [INFO] Downloaded Grounding DINO checkpoint"
+wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth -P $CHECKPOINT_DIR
+echo "* [INFO] Downloaded Grounding DINO checkpoint"
 
 # Orient-Anything (https://github.com/SpatialVision/Orient-Anything)
-#python setup/download_orient_anything.py --cache_dir $CHECKPOINT_DIR
-#echo "* [INFO] Downloaded Orient-Anything checkpoint"
+python setup/download_orient_anything.py --cache_dir $CHECKPOINT_DIR
+echo "* [INFO] Downloaded Orient-Anything checkpoint"
 
 # Depth Pro (https://github.com/apple/ml-depth-pro/blob/main/get_pretrained_models.sh)
-#wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P $CHECKPOINT_DIR
-#echo "* [INFO] Downloaded Depth Pro checkpoint"
+wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P $CHECKPOINT_DIR
+echo "* [INFO] Downloaded Depth Pro checkpoint"
 
 # ================================
